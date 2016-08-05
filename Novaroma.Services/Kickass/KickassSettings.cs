045a89a112e7af1ee11e549ba184fbf19170c956
Emrah Yumuk <emrahyumuk@gmail.com>
2016-08-05 14:30:35 +0300

    Kickass and PirateBay urls changed.

--- Novaroma.Services/Kickass/KickassSettings.cs
+++ Novaroma.Services/Kickass/KickassSettings.cs
@@ -3,7 +3,7 @@
     public class KickassSettings : TorrentProviderSettingsBase {
         internal const string ImdbSearchQuery = "imdb:%imdbId%";
 
-        public KickassSettings(): base("https://kat.cr/") {
+        public KickassSettings(): base("http://kickasstorrentsan.com/") {
             MovieSearchPattern = ImdbSearchQuery;
         }
     }
